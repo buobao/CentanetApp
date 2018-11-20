012e071b6b772e83fa26d2b6aeb7a59efc2b4288
buobao <lebron_king_james@163.com>
2018-11-20 15:23:03 +0800

    沉浸式状态栏

--- app/src/main/java/com/cetnaline/findproperty/base/BaseActivity.java
+++ app/src/main/java/com/cetnaline/findproperty/base/BaseActivity.java
@@ -2,9 +2,7 @@ package com.cetnaline.findproperty.base;
 
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.ColorInt;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -12,11 +10,9 @@ import android.support.annotation.StringRes;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Toast;
 
-import com.cetnaline.findproperty.R;
+import com.cetnaline.findproperty.utils.statusbar.StatusBarUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,15 +26,17 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayout());
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(getStatusBarColor());
-        }else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            // TODO: 2018/11/19
+
+        if (paddingStatusBar()) {
+            StatusBarUtil.setRootViewFitsSystemWindows(this,true);
         }
+
+        //设置状态栏透明
+        StatusBarUtil.setTranslucentStatus(this);
+        if (!StatusBarUtil.setStatusBarDarkTheme(this, true)) {
+            StatusBarUtil.setStatusBarColor(this,0x55000000);
+        }
+
         //设置竖屏
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         ButterKnife.bind(this);
@@ -81,16 +79,15 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     protected abstract @LayoutRes int getLayout();
 
-    /**
-     * 指定状态栏颜色
-     * @return
-     */
-    protected @ColorInt int getStatusBarColor(){
-        return getResources().getColor(R.color.white);
-    }
-
     protected abstract void init(@Nullable Bundle savedInstanceState);
 
+    /**
+     * 设置沉浸式状态栏
+     * @return
+     */
+    protected boolean paddingStatusBar() {
+        return false;
+    }
 
     protected void requestPermission(String[] permissions,@NonNull PermissionListener listener) {
         this.mlistener = listener;
--- app/src/main/java/com/cetnaline/findproperty/ui/SplashActivity.java
+++ app/src/main/java/com/cetnaline/findproperty/ui/SplashActivity.java
@@ -1,10 +1,14 @@
 package com.cetnaline.findproperty.ui;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.WindowManager;
+
+import java.util.logging.Logger;
 
 public class SplashActivity extends AppCompatActivity {
 
